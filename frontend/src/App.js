88338dc4e72b78484504b736fac73cd0bb4ebd1a
Ana Kiperas <z5416105@unsw.edu.au>
2025-02-25 19:29:00 +1100

    set up react project

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import HeaderBar from './HeaderBar/HeaderBar.js'
+
+function App() {
+  return (
+    <HeaderBar />
+  );
+}
+
+export default App;
---
+++ frontend/src/HeaderBar/HeaderBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HeaderBar () {
+  return <h1>Heyyyyy!</h1>
+}
